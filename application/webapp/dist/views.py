3d8908de66fb1fbcabb5dfe0e545400e006a6ce2
plutoese <glen.zhang7@gmail.com>
2016-01-02 10:01:52 +0800

    web version 0.2

--- application/webapp/dist/views.py
+++ application/webapp/dist/views.py
@@ -1,6 +1,6 @@
 # coding=UTF-8
 
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, jsonify
 
 myapp = Blueprint('myapp', __name__)
 
@@ -17,11 +17,101 @@ def updatedlog():
 # 省级数据查询
 @myapp.route('/provincedataquery',methods=['GET', 'POST'])
 def provincedataquery():
+    # 设置数据
     period = range(1990,2015)
-    print(url_for('static', filename='css/multiple-select.css'))
+    regions = [['110100','浙江','温州']]
+    variables = ['国内生产总值','财政收入']
     if request.method == 'POST':
         form_data = request.form
-        #period_chosen = request.form['period']
-        #variables_chosen = request.form['variable']
         print(form_data)
-    return render_template("provincedataquery.html",period=period,regions=[['浙江','温州']],variables=['人均GDP'])
+    return render_template("provincedataquery.html",period=period,regions=regions,variables=variables)
+
+# 省级数据查询
+@myapp.route('/query',methods=['GET', 'POST'])
+def query():
+    # 设置数据
+    period = range(1990,2015)
+    if request.method == 'POST':
+        form_data = request.form
+        print(form_data)
+    return render_template("query.html",period=period)
+
+@myapp.route('/ajaxtwo', methods=['POST','GET'])
+def ajaxtwo():
+    return render_template('ajaxtwo.html')
+
+@myapp.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    #result = a + b
+    return jsonify(result=a + b)
+
+@myapp.route('/ajaxtest', methods=['POST','GET'])
+def ajaxtest():
+    # 设置数据
+    period = range(1990,2015)
+    if request.method == 'POST':
+        print("hello")
+        formdata = request.form
+        period_chosen = formdata.getlist('period')
+        region_chosen = formdata.getlist('region')
+        variables_chosen = formdata.getlist('variable')
+        print(formdata)
+        print(formdata.getlist('period'))
+        print(formdata.getlist('region'))
+        print(formdata.getlist('variable'))
+    return render_template('ajaxtest.html',period=period)
+
+@myapp.route('/jquerylearning',methods=['POST','GET'])
+def jquerylearning():
+    period = range(1990,2015)
+    if request.method == 'POST':
+        print("hello")
+        formdata = request.form
+        print(formdata)
+        print(formdata.getlist('period'))
+        print(formdata.getlist('hregion'))
+        print(formdata.getlist('variable'))
+    return render_template('jquerylearning.html',period=period)
+
+@myapp.route('/_from_year_get_regions',methods=['POST','GET'])
+def from_year_get_regions():
+    period_done = request.values.getlist('period_done[]')
+    # 这里要调用函数，通过时期获取变量
+    region_generated = [24,[{'text':'中国','nodes':[{'text':'北京','nodes':[{'text':'海淀'}]},{'text':'上海'}]}]]
+    return jsonify(regions=region_generated)
+
+@myapp.route('/_from_region_get_variables',methods=['POST','GET'])
+def from_region_get_variables():
+    regions = request.values.getlist('region_selected[]')
+    years = request.values.getlist('period_selected[]')
+    print(regions)
+    print(years)
+    # 这里要调用函数，通过时期和区域获得变量
+    variable_generated = [
+                {
+                    "type": "optiongroup",
+                    "label": "The Griffins",
+                    "children": [
+                        { "type": "option", "value": "Peter",  "label": "Peter Griffin"},
+                        { "type": "option", "value": "Lois",   "label": "Lois Griffin"},
+                        { "type": "option", "value": "Chris",  "label": "Chris Griffin"},
+                        { "type": "option", "value": "Meg",    "label": "Meg Griffin"},
+                        { "type": "option", "value": "Stewie", "label": "Stewie Griffin"}
+                    ]
+                },
+                {
+                    "type": "optiongroup",
+                    "label": "Peter's Friends",
+                    "children": [
+                        { "type": "option", "value": "Cleveland", "label": "Cleveland Brown"},
+                        { "type": "option", "value": "Joe",       "label": "Joe Swanson"},
+                        { "type": "option", "value": "Quagmire",  "label": "Glenn Quagmire"}
+                    ]
+                },
+                { "type": "option", "value": "Evil Monkey", "label": "Evil Monkey"},
+                { "type": "option", "value": "Herbert",     "label": "John Herbert"}
+            ]
+    return jsonify(variables=variable_generated)
---
+++ application/webapp/static/js/pluto-dataquery.js
@@ -0,0 +1,144 @@
+
+/**
+ * @author plutoese <glen.zhang7@gmail.com>
+ * @version 1.0
+ * @function form process
+ * @dependency
+ * @plugin：
+ * @时期多选框：http://wenzhixin.net.cn/p/multiple-select/docs
+ * @区域树形多选框：http://jonmiles.github.io/bootstrap-treeview/
+ * @区域树形多选框：https://github.com/jonmiles/bootstrap-treeview
+ * @变量多选框：http://pbauerochse.github.io/searchable-option-list/examples.html
+ */
+
+$(document).ready(function(){
+	// 初始设置
+	// 设置网站根目录
+
+
+	// 设置区域表单的初始数据
+    var region_defaultData = [
+		{
+			text: '中国',
+			tags:['100000'],
+			selectable: false
+		}
+    ];
+    // 获取变量表单的html，存入变量$variable_form_html
+    var $variable_form_html = $("#variable_form").html();
+
+    // 初始化时期多选框
+    $("select#period").multipleSelect({
+        placeholder: "年份",
+        width: 180,
+        // 事件：关闭选择框
+        onClose: function() {
+        	// 获得选择的时期
+            var period_choosen = $("select#period").multipleSelect('getSelects');
+
+            // 如果选择了时期
+            if (period_choosen.length > 0) {
+	            // 通过ajax获得数据
+	            $.post($SCRIPT_ROOT + '/_from_year_get_regions', {
+	                period_done: period_choosen
+	                }, function(data) {
+	                	// 设置区域表单
+	                	$('#region').treeview({
+	                		// 设置区域数据
+	                    	data: data.regions[1],
+	                    	multiSelect: true,
+	                    	// 定义区域表单中的节点选择，如何选择某个节点，则全选下属一级节点
+	                    	onNodeSelected: function(event,node){
+	                        	var $myselected = $(this);
+                                if (node.state['expanded']) {
+                                    if (node['nodes']) {
+                                        // 选好选择所有的子节点
+                                        $.each(node['nodes'], function (index, value) {
+                                            $myselected.treeview('selectNode', [value['nodeId'], {silent: true}]);
+                                        });
+                                    }
+                                }
+	                    	},
+	                    	// 定义区域表单中的节点选择，如何反选某个节点，则反选所有下属一级节点
+		                    onNodeUnselected: function(event,node){
+		                        var $myunselected = $(this);
+                                if (!node.state['expanded']) {
+                                    if (node['nodes']) {
+                                        $.each(node['nodes'], function (index, value) {
+                                            $myunselected.treeview('unselectNode', [value['nodeId'], {silent: true}]);
+                                        });
+                                    }
+                                }
+		                    }
+	                });
+	                $('#region').treeview('uncheckAll', { silent: true });
+	                $('#region').treeview('collapseAll', { silent: true });
+	                $('#region_done').removeAttr('disabled');
+	         	});
+			}else{
+				$('#region').treeview({
+                    data: region_defaultData,
+                });
+                $('#region_done').attr('disabled','disabled');
+			}
+        },
+    });
+
+	// 设置初始的区域表单
+	$('#region').treeview({
+        data: region_defaultData,
+    });
+
+    // 设置初始的变量表单
+    $("select#variable").searchableOptionList({
+        showSelectAll: true,
+        maxHeight: '250px'
+    });
+
+    // 点击选择区域完成按钮
+    $('#region_done').click(function(){
+    	 // 获取时期和区域选择变量period_choosen和region_choosen
+        var region_nodes = $('#region').treeview('getSelected');
+
+        // 如果选择了区域
+        if (region_nodes.length > 0) {
+            var period_choosen = $("select#period").multipleSelect('getSelects');
+            var region_choosen = [];
+            var i = 0;
+            $.each(region_nodes, function (index, value) {
+                region_choosen[i] = value['text'];
+                i += 1;
+            });
+
+            // 重新设置变量表单，并复制
+            $("#variable_area").remove();
+            $("#variable_form").prepend($variable_form_html);
+            $("#hregion").attr('value', region_choosen);
+
+            // 通过ajax获得变量数据
+            $.post($SCRIPT_ROOT + '/_from_region_get_variables', {
+                region_selected: region_choosen,
+                period_selected: period_choosen
+            }, function (data) {
+                $("select#variable").searchableOptionList({
+                    data: data.variables
+                });
+            });
+            $('#to_be_submit').removeAttr('disabled');
+        }else{
+            $("#variable_area").remove();
+            $("#variable_form").prepend($variable_form_html);
+            // 设置初始的变量表单
+            $("select#variable").searchableOptionList({
+                showSelectAll: true,
+                maxHeight: '250px'
+            });
+            $('#to_be_submit').attr('disabled','disabled');
+        }
+    });
+
+});
+
+
+
+
